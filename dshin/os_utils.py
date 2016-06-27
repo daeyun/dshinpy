8f3b7ae14ee6347c44df2080f75da669ae4d2b07
Daeyun Shin <daeyunshin@gmail.com>
2016-06-26 23:16:13 -0400

    Refactor cleanup

---
+++ dshin/os_utils.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import signal
+import psutil
+import atexit
+import contextlib
+
+
+def kill_children_processes():
+    try:
+        pid = os.getpid()
+        p = psutil.Process(pid)
+        for i in range(3):
+            children = p.children(recursive=True)
+            if len(children) == 0:
+                break
+            for process in children:
+                process.send_signal(signal.SIGKILL)
+        sys.exit()
+    except:
+        pass
+
+
+@contextlib.contextmanager
+def cleanup_on_exit(sig=signal.SIGKILL):
+    os.setpgrp()
+    atexit.register(kill_children_processes)
+    try:
+        yield
+    except (KeyboardInterrupt, SystemExit):
+        os.killpg(os.getpgid(os.getpid()), sig)
