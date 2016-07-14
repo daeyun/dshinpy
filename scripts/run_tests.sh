567b8a05fc87ec02cb05051cca0d5c01df5b3283
Daeyun Shin <daeyunshin@gmail.com>
2016-07-13 20:12:56 -0400

    Update run_tests.sh

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 set -xe
 
 cd $DIR/../
 
-export PYTHONPATH="$HOME/anaconda/lib/python3.5/site-packages/:$PYTHONPATH"
-
-python3 -OO -m unittest discover -s $DIR/../dshin/
+py.test
