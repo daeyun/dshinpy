ed1b7ab585574c88a8e02162d4e485d8f54859ab
Daeyun Shin <daeyunshin@gmail.com>
2016-07-17 17:54:13 -0400

    Set up rendering

---
+++ scripts/setup_matlab.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+[ -z "$MATLAB_ROOT" ] && echo "Need to set MATLAB_ROOT" && exit 1;
+[ -z "$CONDA_ENV" ] && echo "Need to set CONDA_ENV" && exit 1;
+
+set -ex
+
+cd ${MATLAB_ROOT}/extern/engines/python
+source activate ${CONDA_ENV}
+
+
+BUILD_DIR="/tmp/matlab_python_build"
+
+if [[ -d ${BUILD_DIR} ]]; then
+    echo "Removing ${BUILD_DIR}"
+    rm -r ${BUILD_DIR}
+fi
+
+python setup.py build --build-base="${BUILD_DIR}" install
