0d31cbfd3042a1e7255ed833715112504fe608ae
Daeyun Shin <daeyunshin@gmail.com>
2016-07-30 10:30:29 -0400

    Revert types

--- dshin/nn/types.py
+++ dshin/nn/types.py
@@ -5,9 +5,9 @@ import typing
 
 import tensorflow as tf
 
-Value = (tf.Variable, tf.Tensor)
+Value = typing.Union[tf.Variable, tf.Tensor]
 Values = typing.Sequence[Value]
-Named = (tf.Variable, tf.Tensor, tf.Operation)
+Named = typing.Union[tf.Variable, tf.Tensor, tf.Operation]
 NamedSeq = typing.Sequence[Named]
 Tensors = typing.Sequence[tf.Tensor]
 Variables = typing.Sequence[tf.Variable]
