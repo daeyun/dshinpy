429d98e4bda4fe285d1bc2b408b7d59897fb3cc3
Daeyun Shin <daeyunshin@gmail.com>
2016-07-30 10:23:06 -0400

    Update License

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     version='0.1',
     packages=['dshin', 'dshin.nn', 'dshin.tests', 'dshin.third_party'],
     url='https://github.com/daeyun/dshinpy',
-    license='',
+    license='MPL 2.0',
     author='daeyun',
-    author_email='',
+    author_email='daeyun@dshin.org',
     description='',
     tests_require=['pytest'],
 )
