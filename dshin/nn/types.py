cf117771e0e8e54d01853f969394bc552909a90c
Daeyun Shin <daeyunshin@gmail.com>
2016-07-30 10:28:42 -0400

    Update type annotations

--- dshin/nn/types.py
+++ dshin/nn/types.py
@@ -5,9 +5,9 @@ import typing
 
 import tensorflow as tf
 
-Value = typing.Union[tf.Variable, tf.Tensor]
+Value = (tf.Variable, tf.Tensor)
 Values = typing.Sequence[Value]
-Named = typing.Union[tf.Variable, tf.Tensor, tf.Operation]
+Named = (tf.Variable, tf.Tensor, tf.Operation)
 NamedSeq = typing.Sequence[Named]
 Tensors = typing.Sequence[tf.Tensor]
 Variables = typing.Sequence[tf.Variable]
