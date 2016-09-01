cf73022cb63e11a2899fc182298facdcba1881b3
Daeyun Shin <daeyunshin@gmail.com>
2016-09-01 18:56:54 -0500

    Add tensorflow graph debugging code.

--- dshin/nn/graph.py
+++ dshin/nn/graph.py
@@ -1,54 +1,139 @@
-import abc
-import typing
-
 import tensorflow as tf
 
+import collections
+import numpy as np
+import textwrap
 
-class Builder(metaclass=abc.ABCMeta):
-    def __init__(self, name: str, in_size: int = None, out_size: int = None,
-                 in_dims: int = None, out_dims: int = None,
-                 is_bn_local=True, is_training: tf.Tensor = None, is_first=False, is_last=False,
-                 has_activation=True, has_weight=True, has_bn=True):
+from IPython.display import display, HTML
+
+
+class IPythonTfGraph(object):
+    """
+    Display TensorFlow graphs in Jupyter. http://stackoverflow.com/a/38192374
+    """
+
+    def __init__(self, graph):
+        self.graph = graph
+
+    def strip_consts(self, graph_def, max_const_size=32):
         """
-        Building blocks for Tensorflow graphs. in_size and out_size are
-        optional, but usually one of them should be provided.
-
-        :param name: Variable scope name.
-        :param in_size: Input resolution is [in_size]*in_dims.
-        :param out_size: Output resolution is [out_size]*out_dims.
-        :param is_bn_local: A Tensor of type tf.bool or a constant bool. Not all builders need this.
-        :param is_training: A Tensor of type tf.bool. Not all builders need this.
+        Strip large constant values from graph_def.
         """
-        if is_training is not None:
-            assert isinstance(is_training, tf.Tensor) and is_training.dtype == tf.bool
-        assert not (is_first and is_last)
-        assert has_activation or has_weight
-        assert isinstance(in_dims, int) or isinstance(out_dims, int)
-        assert isinstance(is_last, bool)
-        assert isinstance(is_first, bool)
-        assert isinstance(has_activation, bool)
-        assert isinstance(has_weight, bool)
-        assert isinstance(has_bn, bool)
+        strip_def = tf.GraphDef()
+        for n0 in graph_def.node:
+            n = strip_def.node.add()
+            n.MergeFrom(n0)
+            if n.op == 'Const':
+                tensor = n.attr['value'].tensor
+                size = len(tensor.tensor_content)
+                if size > max_const_size:
+                    tensor.tensor_content = "<stripped %d bytes>" % size
+        return strip_def
 
-        self.name = name
-        self.in_size = in_size
-        self.out_size = out_size
-        self.is_bn_local = is_bn_local
-        self.is_training = is_training
-        self.is_first = is_first
-        self.is_last = is_last
-        self.has_activation = has_activation
-        self.has_weight = has_weight
-        self.has_bn = has_bn
+    def rename_nodes(self, graph_def, rename_func):
+        res_def = tf.GraphDef()
+        for n0 in graph_def.node:
+            n = res_def.node.add()
+            n.MergeFrom(n0)
+            n.name = rename_func(n.name)
+            for i, s in enumerate(n.input):
+                n.input[i] = rename_func(s) if s[0] != '^' else '^' + rename_func(s[1:])
+        return res_def
 
-    def build(self, input_value: tf.Tensor) -> tf.Tensor:
+    def show(self, graph_or_graph_def=None, max_const_size=32):
         """
-        Defines operations and variables in the current graph.
+        Visualize TensorFlow graph.
         """
-        with tf.variable_scope(self.name):
-            return self._build(input_value)
+        if graph_or_graph_def is None:
+            graph_or_graph_def = self.graph
 
-    @abc.abstractmethod
-    def _build(self, value: typing.Union[tf.Tensor, typing.List[tf.Tensor]]) -> tf.Tensor:
-        pass
+        if hasattr(graph_or_graph_def, 'as_graph_def'):
+            graph_or_graph_def = graph_or_graph_def.as_graph_def()
 
+        strip_def = self.strip_consts(graph_or_graph_def, max_const_size=max_const_size)
+        code = """
+            <script>
+              function load() {{
+                document.getElementById("{id}").pbtxt = {data};
+              }}
+            </script>
+            <link rel="import" href="https://tensorboard.appspot.com/tf-graph-basic.build.html" onload=load()>
+            <div style="height:600px">
+              <tf-graph-basic id="{id}"></tf-graph-basic>
+            </div>
+        """.format(data=repr(str(strip_def)), id='graph' + str(np.random.rand()))
+        code = textwrap.dedent(code)
+
+        iframe = """<iframe seamless style="width:800px;height:620px;border:0" srcdoc="{}"></iframe>""".format(code.replace('"', '&quot;'))
+
+        display(HTML(iframe))
+
+
+def backtrace_invalid(tensor: tf.Tensor, feed_dict, maxdepth=2):
+    if maxdepth < 0:
+        return
+
+    sess = tf.get_default_session()
+    assert sess is not None
+
+    def count_invalid_single(tensor):
+        eval_values = sess.run(tensor, feed_dict=feed_dict)
+        values, counts = np.unique(eval_values[~np.isfinite(eval_values)], return_counts=True)
+        total = eval_values.size
+        return values, counts, total
+
+    def count_invalid(tensors):
+        if isinstance(tensors, (list, tuple)):
+            return [count_invalid_single(t) for t in tensors]
+        else:
+            return count_invalid_single(tensors)
+
+    queue = collections.deque()
+    invalid_counts = count_invalid(tensor)
+    current_depth = 0
+    if invalid_counts[1].sum() > 0:
+        queue.append([tensor, current_depth, invalid_counts])
+
+    results = []
+
+    while len(queue) > 0:
+        parent, d, counts = queue.popleft()
+        parent_invalid_values, parent_invalid_counts, parent_total_count = counts
+        parent_invalid_count = parent_invalid_counts.sum()
+
+        children = []
+        has_all_valid_inputs = True
+        child_invalid_counts = count_invalid(list(parent.op.inputs))
+        for child, (invalid_values, invalid_counts, total_count) in zip(list(parent.op.inputs), child_invalid_counts):
+            invalid_count = invalid_counts.sum()
+            children.append([child, invalid_count])
+            if invalid_count > 0:
+                has_all_valid_inputs = False
+                if d < maxdepth:
+                    if d != current_depth or current_depth == 0:
+                        current_depth = d
+                        if current_depth != 0:
+                            print()
+                        print('Searching level {} {}'.format(d, parent.name), end='')
+                    else:
+                        print('.', end='')
+                    queue.append([child, d + 1, (invalid_values, invalid_counts, total_count)])
+        print(' ', end='')
+
+        if has_all_valid_inputs:
+            results.append(parent)
+            print()
+            print('{} invalid values out of {} in {} at depth {}'.format(parent_invalid_count, parent_total_count, parent.name, d))
+            if len(parent_invalid_values) > 1:
+                for i in range(len(parent_invalid_values)):
+                    print('{}: {}'.format(parent_invalid_values[i], parent_invalid_counts[i]))
+            print('Op: {}'.format(parent.op.type))
+            print('Inputs:')
+            if children:
+                for child, invalid_count in children:
+                    print('{} - {} of shape {} and dtype {}'.format(child.name, type(child).__name__, child.get_shape(), child.dtype.name))
+            else:
+                print('No input values.')
+            print()
+
+    return results
